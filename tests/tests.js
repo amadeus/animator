4fceca8e6ef802277b21338a324cfd69d7b47d81
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2015-03-18 16:12:42 -0700

    Improvements to tests

--- tests/tests.js
+++ tests/tests.js
@@ -149,27 +149,49 @@ pauseResume.addEventListener('click', function(event){
 
 var element = document.getElementById('animate');
 
+element.style.opacity = 0;
 element.style[Animator.findPrefix('transform')] = 'rotate(45deg) translate3d(0, 20px, 10px)';
+element.style.background = 'rgba(255,0,0,1)';
 
-animator
-	.tweenElement(element, 500, {
-		_timing: 'ease-in-out',
-		backgroundColor: {
-			rgb: [0,0,0]
-		},
-		opacity: 1,
-		transform: {
-			rotate: '0deg'
-		}
-	}).tweenElement(element, 500, {
-		_timing: 'ease-in-out',
-		opacity: 0,
-		backgroundColor: {
-			rgb: [255,0,176]
-		},
-		transform: {
-			translate3d: [0, 200, 0]
-		}
-	});
+
+animator.tweenElement(element, 500, {
+	_timing: 'ease-in-out',
+	backgroundColor: {
+		rgb: [0,0,0]
+	},
+	opacity: 1,
+	transform: {
+		rotate: '0deg'
+	}
+});
+
+animator.tweenElement(element, 500, {
+	_timing: 'ease-in-out',
+	opacity: 0.5,
+	backgroundColor: {
+		rgb: [255,0,176]
+	},
+	transform: {
+		translate3d: [0, 200, 0]
+	}
+});
+
+animator.tweenElement(element, 500, {
+	_timing: 'ease-in-out',
+	borderRadius: [100]
+});
+
+animator.tweenElement(element, 500, {
+	_timing: 'ease-in-out',
+	borderRadius: [0],
+	opacity: 1,
+	'background-color': {
+		rgba: [255,0,0,1]
+	},
+	transform: {
+		translate3d: [0, 0, 0]
+	}
+});
+
 
 })();
