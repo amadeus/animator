a0c1197588bad6c7d8ab43c249c25e9259cc9d9c
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2015-04-30 15:52:39 -0700

    Improved scene example
    Scene example now implements more types, along with instantiating a
    scene recursively

--- tests/scene.js
+++ tests/scene.js
@@ -1,108 +1,115 @@
 (function(){
 
+var popin = {
+	0: {
+		opacity: 0,
+		transform: {
+			translate3d: [0,500, 0]
+		},
+		_timing: Animator.TWEENS.EASE_OUT_SINE,
+	},
+	40: {
+		opacity: 1,
+		transform: {
+			translate3d: [0,-60,0]
+		},
+		_timing: Animator.TWEENS.EASE_OUT_SINE
+	},
+	70: {
+		transform: {
+			translate3d: [0,20,0]
+		},
+		_timing: Animator.TWEENS.EASE_OUT_SINE,
+	},
+	100: {
+		transform: {
+			translate3d: [0,0,0]
+		}
+	}
+};
+
 var scene = [
 	{
 		start: 0,
+		queue: {
+			element: document.body,
+			duration: 700,
+			to: {
+				opacity: 1
+			}
+		}
+	}, {
+		start: 200,
 		queue: [
 			{
 				element: 'first',
-				duration: 1000,
-				from: {
-					transform: {
-						translate3d: [0, 0, 0]
-					},
-					_timing: 'ease-in-out'
-				},
-				to: {
-					transform: {
-						translate3d: [0, 200, 0]
-					}
-				}
+				duration: 300,
+				animation: 'popin'
 			}, {
-				element: 'second',
-				duration: 500,
-				from: {
-					transform: {
-						translate3d: [0, 0, 0]
-					}
-				},
-				to: {
-					transform: {
-						translate3d: [300, 300, 0]
-					},
-					_timing: 'ease-in-out'
-				}
+				scene: 'pulse-scene'
 			}
 		]
 	}, {
-		start: 400,
-		queue: [
-			{
-				scene: 'block-scene2'
-			}
-		]
-	},
-	{
-		start: 400,
-		queue: [
-			{
-				element: 'third',
-				duration: 1000,
-				to: {
-					transform: {
-						translate3d: [100, 0, 0]
-					},
-					_timing: 'ease-in-out'
-				}
-			}
-		]
-	}, {
-		start: 0,
+		start: 200,
 		queue: {
-			element: document.body,
-			duration: 1000,
+			element: 'first',
+			duration:300,
 			from: {
-				backgroundColor: {
-					rgb: [255, 255, 255]
-				},
-				_timing: 'ease-in-out'
+				opacity: 0
 			},
 			to: {
-				backgroundColor: {
-					rgb: [100, 100, 100]
-				}
+				opacity: 1
 			}
 		}
 	}
 ];
 
-var scene2 = {
-	start: 0,
-	queue: [
-		{
-			element: document.body,
-			duration: 500,
-			to: {
-				transform: {
-					scale: [0.5, 0.5],
+var pulseScene = [
+	{
+		start: 0,
+		queue: [
+			{
+				element: 'first',
+				duration: 150,
+				from: {
+					transform: {
+						scale: [1, 1]
+					},
+					_timing: 'ease-in-out'
 				},
-				_timing: 'ease-in'
-			}
-		}, {
-			element: document.body,
-			duration: 500,
-			to: {
-				transform: {
-					scale: [1, 1],
+				to: {
+					transform: {
+						scale: [1.1, 1.1]
+					}
+				}
+			}, {
+				element: 'first',
+				duration: 400,
+				from: {
+					transform: {
+						scale: [1.1, 1.1]
+					},
+					_timing: 'ease-in-out'
 				},
-				_timing: 'ease-out'
+				to: {
+					transform: {
+						scale: [1, 1]
+					},
+					_finished: function(){
+						Animator.runScene('pulse-scene');
+					}
+				}
 			}
-		}
-	]
-};
+		]
+	}
+];
 
+Animator.createAnimation('popin', popin);
 Animator.createScene('block-scene', scene);
-Animator.createScene('block-scene2', scene2);
-Animator.runScene('block-scene');
+Animator.createScene('pulse-scene', pulseScene);
+
+setTimeout(function() {
+	Animator.runScene('block-scene');
+}, 500);
 
 })();
