dd76cfa111cfcf21441806caf7dff38f62955906
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2015-05-11 17:55:05 -0700

    Added starting velocity to springs

--- src/animator.js
+++ src/animator.js
@@ -771,7 +771,7 @@ Internal = {
 
 		for (name in reference.target) {
 			settings.current[name] = (reference.start) ? reference.start[name] : reference.target[name];
-			settings.vel[name]     = 0;
+			settings.vel[name]     = (reference.vel) ? reference.vel[name] : 0;
 			settings.accel[name]   = 0;
 		}
 
